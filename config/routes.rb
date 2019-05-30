9ae2eeb19977207c842870465cecc7885c39b971
遠藤一穂 <endoukazuho@endouichihonoMacBook-Pro.local>
2019-05-30 14:12:02 +0900

    7-5

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
 
   get 'signup', to: 'users#new'
+  post '/signup',  to: 'users#create' # 「リスト 7.26」で追加
 
+  resources :users
+    #ユーザー情報を表示するURL（/users/1）を追加するだけではない
+    #RESTfulなUsersリソースで必要となる全てのアクションが利用できるようになる
 
 end
