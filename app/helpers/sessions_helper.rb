350c7e3ddf17eefe7b68edcf54f74728dd70eb99
遠藤一穂 <endoukazuho@endouichihonoMacBook-Pro.local>
2019-05-31 19:01:06 +0900

    9章

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,10 +5,24 @@ module SessionsHelper
     session[:user_id] = user.id # ユーザーIDをsessionのuser_idに代入（ログインIDの保持）
   end
 
-  # 現在ログイン中のユーザー（current_user）を返す（いる場合）
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # 現在ログイン中のユーザー（current_user）を返す（いる場合）&記憶トークンcookieに対応するユーザーを返す
   def current_user
-    if session[:user_id] # ログインユーザーがいたらtrue処理
-      @current_user ||= User.find_by(id: session[:user_id]) # ログインユーザーがいればそのまま、いなければcookieのユーザーと同じidを持つユーザーをDBから探して@current_userに代入
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      #raise  # 「raise」を置くことでわざとエラーをだす。もしテストがパスすれば、この部分がテストされていないことがわかる
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
@@ -17,8 +31,16 @@ module SessionsHelper
     !current_user.nil? # current_user（ログインユーザー）がnilじゃ無いならtrue、それ以外はfalseを返す。!を先頭に付けることによって、否定演算子(not)を使い、本来ならnilならtrueの所をnilじゃないならtrueにしている。
   end
 
+  #永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # 現在のユーザーをログアウトする
   def log_out
+    forget(current_user)
     session.delete(:user_id)  # sessionのユーザーIDを削除する
     @current_user = nil       # 現在のログインユーザー（一時的なcookie）をnil（空に）する
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase } #DBに保存する前にemail属性を強制的に小文字に変換する
 
   validates :name, presence: true, length: { maximum: 50 } #nameは空は許さず、50文字以下
@@ -18,6 +20,28 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                    BCrypt::Engine.cost
      BCrypt::Password.create(string, cost: cost)
+   end
+
+   # ランダムなトークンを返す
+   def User.new_token
+     SecureRandom.urlsafe_base64
+   end
+
+   # 永続的セッションのためにユーザーをデータベースに記憶させる
+   def remember
+     self.remember_token = User.new_token
+     update_attribute(:remember_digest, User.digest(remember_token))
+   end
+
+   # 渡されたトークンがダイジェストと一致したらtrueを返す
+   def authenticated?(remember_token)
+     return false if remember_digest.nil?
+     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+   end
+
+   # ユーザーのログイン情報を破棄する
+   def forget
+     update_attribute(:remember_digest, nil)
    end
 
 end
