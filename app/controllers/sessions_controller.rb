4f5ee946acf2ea07c1139dda73bdab0eee2f15dc
遠藤一穂 <endoukazuho@endouichihonoMacBook-Pro.local>
2019-06-04 20:07:37 +0900

    11-3まで

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:session][:email].downcase) #paramsハッシュで受け取ったemail値を小文字化して、email属性に渡してUserモデルから同じemailの値のuserを探して、user変数に代入
-        # 上記のparams[:session]を細かく見ると → { session: { params: "foobar", email: "user@example.com"} }
-    if @user && @user.authenticate(params[:session][:password]) #user変数がDBに存在し（&&で判別）、尚且つparamsハッシュを受け取ったpassword値とuserのemail値が同じであればtrue
-      log_in @user #session_helperのlog_inメソッドを実行し、sessionメソッドのuser_id（ブラウザに一時coolieとして保持）にidを送る
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_back_or @user 
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      if @user.activated?
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or @user
+      else
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = "メールアドレス/パスワードが間違えています"
       render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,19 +8,22 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.paginate(page: params[:page])
+#    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
 
   def create
     @user = User.new(user_params)
     if @user.save #保存の成功をここで扱う
-      log_in @user
-      flash[:success] = "ようこそSample Appへ"
-      redirect_to @user #userのプロフィールページへリダイレクト
+      @user.send_activation_email
+#      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "確認メールを送信しました。アカウントを有効にするには、メールをご確認ください。"
+      redirect_to root_url # ルートurlにリダイレクト
     else
       render 'new'
     end
